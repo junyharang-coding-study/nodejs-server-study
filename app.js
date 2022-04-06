a8913a716b1b070f6bd3fa0c1f1d82053180432d
junyharang <junyharang8592@gmail.com>
2022-04-06 16:40:58 +0900

    Express Template Engine ejs 공부 완료

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 8080;
 app.use(express.static('public'));   // public이라는 Directory를 static(정적)으로 기억하라고 명령
 app.use(bodyPaser.json());                  // express Server에 나! Body Paser쓸거야! 라고 알려주는 명령이면서 Json으로 Client의 Data를 받겠다는 의미
 app.use(bodyPaser.urlencoded({extended:true}));     // client에 오는 요청이 json 형태가 아닐 때 urlencoding으로 Data를 받겠다는 의미
+app.set("view engine", "ejs");                      // express에게 view engine은 ejs를 쓰겠다고 명령! 설치법 : npm install ejs --save
 
 app.listen(port, function() {
     console.log(`start Node Server! on port ${port}`);
@@ -30,6 +31,8 @@ app.post("/email_post", function(request, response) {
     // post방식은 get과 같이 Data를 받을 수 없고, bodyPaser를 통해 Data를 받아야 하므로, 설치 필요
     // 설치 명령어 : npm install body-paser --save
     console.log(request.body.email);
-    response.send(`<h1>Welcome ${request.body.email}! 👋</h1> <br> `);
+    // response.send(`<h1>Welcome ${request.body.email}! 👋</h1>`);
+
+    response.render('email.ejs', {'email' : request.body.email});
 
 });
