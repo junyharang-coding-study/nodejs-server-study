dc3d36a656408ee301fbbfd41ef37f79d16f5a39
junyharang <junyharang8592@gmail.com>
2022-04-06 16:12:55 +0900

    Get 요청 학습 완료

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const app = express();
 
 const port = 8080;
 
+app.use(express.static('public'))   // public이라는 Directory를 static(정적)으로 기억하라고 명령
+
 app.listen(port, function() {
     console.log(`start Node Server! on port ${port}`);
 });
 
-app.get("/", function(request, response) {
+app.get("/", function(request, response) {  // url Routing
 
     // response.send("<h1>Hello JunyHarang 👋</h1>");
+
+    response.sendFile(__dirname + "/public/main.html")
+
+});
+
+app.get("/main", function(request, response) {  // url Routing
 
     response.sendFile(__dirname + "/public/main.html")
 
